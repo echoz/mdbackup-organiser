d999da6281ccb241c920cd6cbc78deb7b7d7a44d
Jeremy <jeremyfoo@gmail.com>
2010-04-10 14:08:49 +0800

    Initial import of code

---
+++ extract.m
@@ -0,0 +1,76 @@
+#import <Foundation/Foundation.h>
+#import "NSDataAdditions.h"
+
+int main (int argc, const char * argv[]) {
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+		
+	NSString *srcPath = @"/Users/jeremy/ac355f572185eeab8adc348922bba9a9fd25c23a/";
+	NSString *destPath = @"/Users/jeremy/backup/";
+	
+	
+	/* -----------------------------------------------------------------------------
+
+	 DO NOT EDIT ANYTHING BELOW THIS UNLESS YOU KNOW WHAT YOU ARE DOING.
+	 
+	 ----------------------------------------------------------------------------- */
+	
+	NSDictionary *manifest = [NSPropertyListSerialization propertyListWithData:[[NSDictionary dictionaryWithContentsOfFile:[NSString stringWithFormat:@"%@Manifest.plist", srcPath]] valueForKey:@"Data"]
+																	   options:NSPropertyListImmutable format:nil error:nil];
+	
+	NSDictionary *applications = [manifest objectForKey:@"Applications"];
+	NSMutableDictionary *files = [[manifest objectForKey:@"Files"] mutableCopy];
+	
+	NSFileManager *fm = [NSFileManager defaultManager];	
+	
+	/* lets do applications first */
+	int i;
+	NSString *currDest;
+	NSArray *currArray;
+	
+	NSLog(@"Dealing with Application backup files now");
+	
+	for (id key in applications) {
+		currDest = [NSString stringWithFormat:@"%@Applications/%@",destPath,key];
+		currArray = [[applications objectForKey:key] objectForKey:@"Files"];
+		
+		NSLog(@"\tDealing with %@ now...", key);
+		[fm createDirectoryAtPath:currDest withIntermediateDirectories:YES attributes:nil error:nil];		
+		
+		for (i=0;i<[currArray count];i++) {
+			NSLog(@"\t\tMoving %@ now...", [currArray objectAtIndex:i]);
+			[fm moveItemAtPath:[NSString stringWithFormat:@"%@%@.mdinfo",srcPath,[currArray objectAtIndex:i]]
+				  toPath:[NSString stringWithFormat:@"%@%@.mdinfo",currDest,[currArray objectAtIndex:i]]
+				   error:nil];
+			[fm moveItemAtPath:[NSString stringWithFormat:@"%@%@.mddata",srcPath,[currArray objectAtIndex:i]]
+				  toPath:[NSString stringWithFormat:@"%@%@.mddata",currDest,[currArray objectAtIndex:i]]
+				   error:nil];
+			[files removeObjectForKey:[currArray objectAtIndex:i]];
+
+		}
+	}
+	
+	/* and all other files now */
+	
+	NSLog(@"Dealing with other backup files now");	
+	
+	BOOL isDir;
+	
+	for (id key in files) {
+		currDest = [NSString stringWithFormat:@"%@Files/%@/", destPath, [[files objectForKey:key] objectForKey:@"Domain"]];
+		
+		if (!([fm fileExistsAtPath:currDest isDirectory:&isDir] && isDir) ) {
+			[fm createDirectoryAtPath:currDest withIntermediateDirectories:YES attributes:nil error:nil];				
+		}
+		NSLog(@"Moving %@ to %@", key, [[files objectForKey:key] objectForKey:@"Domain"]);
+		[fm moveItemAtPath:[NSString stringWithFormat:@"%@%@.mdinfo",srcPath,key]
+					toPath:[NSString stringWithFormat:@"%@%@.mdinfo",currDest,key]
+					 error:nil];
+		[fm moveItemAtPath:[NSString stringWithFormat:@"%@%@.mddata",srcPath,key]
+					toPath:[NSString stringWithFormat:@"%@%@.mddata",currDest,key]
+					 error:nil];		
+		
+	}
+
+	[pool drain];
+    return 0;
+}
